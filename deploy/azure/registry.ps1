d2b5b354989ad81e37969b35d468a5182c7738b6
Siarhei-Sialitski <siarhei.sialitski.ws@gmail.com>
2023-02-03 17:41:56 +0100

    comments

--- deploy/azure/registry.ps1
+++ deploy/azure/registry.ps1
@@ -1,9 +1,3 @@
-# Write-Host "DNS base found is $dns. Will use $appName.$dns for the app!" -ForegroundColor Green
-
-# docker tag eshop/webshoppingagg:linux-latest crtshapess.azurecr.io/eshop/webshoppingagg:linux-latest
-# docker push crtshapess.azurecr.io/eshop/webshoppingagg:linux-latest
-
-
 $registry = "crtshapess.azurecr.io"
 $apps = ("basket.api", "catalog.api", "coupon.api", "identity.api", "mobileshoppingagg", "ordering.api", "ordering.backgroundtasks", "ordering.signalrhub", "payment.api", "webmvc", "webshoppingagg", "webspa", "webstatus", "webhooks.api", "webhooks.client")
 
